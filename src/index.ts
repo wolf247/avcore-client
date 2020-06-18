8c00d95cc08945587406a142ae5b3142bc822132
Alexander Kiyashko <angedonik@gmail.com>
2020-06-18 15:05:19 +0300

    frontend

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,5 @@
 export {ERROR,ACTION,API_OPERATION} from './constants';
 export {MediasoupSocketApi} from './mediasoup-socket-api';
 export {IMediasoupApi} from './i-mediasoup-api';
-
-/*
-import {Utils} from './utils';
-import {ConferenceApi} from './conference-api';
-import {ERROR} from './constants';
-import {MediasoupSocketApi} from './mediasoup-socket-api';
-import * as debug  from 'debug';
-if(typeof window !== 'undefined'){
-    (window as any).debug=debug;
-    (window as any).Utils=Utils;
-    (window as any).ConferenceApi=ConferenceApi;
-    (window as any).ERROR=ERROR;
-    (window as any).MediasoupRestApi=MediasoupSocketApi;
-    (window as any).MediasoupSocketApi=MediasoupSocketApi;
-
-}*/
-
+export {Utils} from './utils';
+export {ConferenceApi} from './conference-api';
