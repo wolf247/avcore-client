c6142918f4b532eb682687292cec1c7b1428ba0b
Alexander Kiyashko <angedonik@gmail.com>
2020-06-19 08:02:31 +0300

    exports

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,3 +3,5 @@ export { MediasoupSocketApi } from './mediasoup-socket-api';
 export { IMediasoupApi } from './i-mediasoup-api';
 export { Utils } from './utils';
 export { ConferenceApi } from './conference-api';
+import * as interfaces from './client-interfaces';
+export { interfaces };
--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export {MediasoupSocketApi} from './mediasoup-socket-api';
 export {IMediasoupApi} from './i-mediasoup-api';
 export {Utils} from './utils';
 export {ConferenceApi} from './conference-api';
-export * from './client-interfaces';
+import * as interfaces from './client-interfaces';
+export { interfaces };
