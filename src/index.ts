e52688334c224222cea78945d45c16950c137414
Alexander Kiyashko <angedonik@gmail.com>
2020-06-19 08:09:02 +0300

    exports

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,4 @@ export {MediasoupSocketApi} from './mediasoup-socket-api';
 export {IMediasoupApi} from './i-mediasoup-api';
 export {Utils} from './utils';
 export {ConferenceApi} from './conference-api';
-import * as interfaces from './client-interfaces';
-export { interfaces };
+export * from './client-interfaces';
