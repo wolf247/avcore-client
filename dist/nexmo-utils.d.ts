43af2973e810e360a0c372ba69fd0166b1e11d56
Alexander Kiyashko <angedonik@gmail.com>
2020-07-09 16:35:57 +0300

    update

---
+++ dist/nexmo-utils.d.ts
@@ -0,0 +1,51 @@
+export declare class NexmoUtils {
+    static pinCodeChoice(maxDigits: number, eventUrl: string, text?: string): ({
+        action: string;
+        text: string;
+        bargeIn: boolean;
+        maxDigits?: undefined;
+        timeOut?: undefined;
+        eventUrl?: undefined;
+    } | {
+        action: string;
+        maxDigits: number;
+        timeOut: number;
+        eventUrl: string[];
+        text?: undefined;
+        bargeIn?: undefined;
+    })[];
+    static pinCodeChoiceRepeat(text: string | undefined, maxDigits: number, eventUrl: string, pinCodeChoiceText?: string): ({
+        action: string;
+        text: string;
+        bargeIn: boolean;
+        maxDigits?: undefined;
+        timeOut?: undefined;
+        eventUrl?: undefined;
+    } | {
+        action: string;
+        maxDigits: number;
+        timeOut: number;
+        eventUrl: string[];
+        text?: undefined;
+        bargeIn?: undefined;
+    } | {
+        action: string;
+        text: string;
+    })[];
+    static mixerConnect(url: string, worker: number, mixerId: string): ({
+        action: string;
+        text: string;
+        endpoint?: undefined;
+    } | {
+        action: string;
+        endpoint: {
+            type: string;
+            uri: string;
+            "content-type": string;
+            headers: {
+                mixerId: string;
+            };
+        }[];
+        text?: undefined;
+    })[];
+}
