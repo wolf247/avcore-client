384bf3dec7db107592ca6325b2f6b287327894b4
Alexander Kiyashko <angedonik@gmail.com>
2020-07-22 17:49:04 +0300

    update

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
-export { ERROR, ACTION, API_OPERATION, EVENT, STAT, MIXER_PIPE_TYPE, NEXMO } from './constants';
+export { ERROR, ACTION, API_OPERATION, EVENT, STAT, MIXER_PIPE_TYPE, NEXMO, HLS } from './constants';
 export { MediasoupSocketApi } from './mediasoup-socket-api';
 export { IMediasoupApi } from './i-mediasoup-api';
 export * from './client-interfaces';
