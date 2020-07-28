6b5ad881b8c1f85302c4b6adf5db76d4a7f2b87a
Alexander Kiyashko <angedonik@gmail.com>
2020-07-28 13:53:40 +0300

    update

--- webpack.config.js
+++ webpack.config.js
@@ -3,51 +3,50 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = env => {
-  return {
-    mode: env.production ? 'production' : 'development',
-    entry: './src/index.ts',
-    output: {
-      filename: 'index.js',
-      path: path.resolve(__dirname, 'dist'),
-      library: 'avcore',
-      libraryTarget: 'umd',
-      globalObject: 'this',
-    },
-    devtool: env.production ? false : 'cheap-module-eval-source-map',
-    plugins: [new CleanWebpackPlugin()],
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: [
-            {
-              loader: 'ts-loader'
-            },
-          ],
+    return {
+        mode: env.production ? 'production' : 'development',
+        entry: './src/index.ts',
+        output: {
+            filename: 'index.js',
+            path: path.resolve(__dirname, 'dist'),
+            library: 'avcore',
+            libraryTarget: 'umd',
+            globalObject: 'this',
         },
-      ],
-    },
-    resolve: {
-      extensions: [ '.tsx', '.ts', '.js' ],
-    },
-    optimization: env.production ? {
-      minimize: true,
-      minimizer: [new TerserPlugin()],
-    } : {},
-    externals: {
-      'socket.io-client': {
-        root: 'io',
-        commonjs: 'socket.io-client',
-        commonjs2: 'socket.io-client',
-        amd: 'socket.io-client',
-      },
-      'rxjs': 'rxjs',
-      'rxjs/operators': {
-        root: ['rxjs', 'operators'],
-        commonjs: 'rxjs/operators',
-        commonjs2: 'rxjs/operators',
-        amd: 'rxjs/operators',
-      },
-    },
-  };
+        devtool: env.production ? false : 'cheap-module-eval-source-map',
+        plugins: [new CleanWebpackPlugin()],
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: [
+                        {
+                            loader: 'ts-loader'
+                        },
+                    ],
+                },
+            ],
+        },
+        resolve: {
+            extensions: [ '.tsx', '.ts', '.js' ],
+        },
+        optimization: env.production ? {
+            minimize: true,
+            minimizer: [new TerserPlugin()],
+        } : {},
+        externals: {
+            'socket.io-client': {
+                root: 'io',
+                commonjs: 'socket.io-client',
+                commonjs2: 'socket.io-client',
+                amd: 'socket.io-client',
+            },
+            'axios': {
+                root: 'axios',
+                commonjs: 'axios',
+                commonjs2: 'axios',
+                amd: 'axios',
+            },
+        },
+    };
 };
