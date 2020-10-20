20fd468312d654d80a1ad50f8cc8e296821f5709
SergeyPostupaiev <postupaiev.sergey@gmail.com>
2020-10-20 15:20:19 +0300

    push-script changed

--- push-script.sh
+++ push-script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-chmod +x ./push-script.sh
 
 
 elements=("src/" "client/src/" "push-script.sh" "package-lock.json" "test.ts" "tsconfig.json" "webpack.config.js" "**/package-lock.json" "**/test.ts" "**/tsconfig.json" "**/webpack.config.js")
@@ -8,7 +7,9 @@ tmpDir=$(mktemp -d /tmp/git-copy-XXXXXX)
 cd ${tmpDir}
 
 # from
-git clone git@github.com:anovikov1984/avcore.git .
+git clone git@github.com:anovikov1984/avcore.git 
+cd avcore
+
 git checkout -b temp
 
 for var in "${elements[@]}"
@@ -26,5 +27,21 @@ git push -u tempremote +temp:main
 git checkout master
 git branch -D temp
 git remote rm tempremote
+
+cd ..
+
+# update version
+git clone git@github.com:codeda/avcore-client.git
+cd avcore-client
+
+VERSION=$(npm view avcore version)
+IFS="." read -ra NUMS <<< "$VERSION"
+((NUMS[2]++))
+
+UPD_VER=$( IFS=$'.'; echo "${NUMS[*]}" )
+
+npm version $UPD_VER
+npm publish
+
 
 rm -rf "${tmpDir}"
