d5c37a011b4f6eab7aa66567453f5945a4752655
SergeyPostupaiev <postupaiev.sergey@gmail.com>
2020-10-20 11:36:40 +0300

    push-script added

---
+++ push-script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+chmod +x ./script.sh
+
+
+elements=("src/" "client/src/" "push-script.sh" "package-lock.json" "test.ts" "tsconfig.json" "webpack.config.js")
+
+tmpDir=$(mktemp -d /tmp/git-copy-XXXXXX)
+cd ${tmpDir}
+
+# from
+git clone git@github.com:anovikov1984/avcore.git .
+git checkout -b temp
+
+for var in "${elements[@]}"
+do
+  echo $var >> .gitignore
+  git rm --cached $var -r
+done
+
+# to
+git remote add tempremote git@github.com:codeda/avcore-client.git
+git add .
+git commit -m 'remote commit'
+git push -u tempremote +temp:main
+
+git checkout master
+git branch -D temp
+git remote rm tempremote
+
+rm -rf "${tmpDir}"
